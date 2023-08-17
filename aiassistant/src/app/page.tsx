30d505840fd93cef4e23074b58b13aa9ef3ee6bb
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2023-08-17 13:22:54 -0700

    Update page.tsx

--- aiassistant/src/app/page.tsx
+++ aiassistant/src/app/page.tsx
@@ -1,4 +1,4 @@
-import SubmissionForm from "./submission/SubmissionForm";
+import SubmissionForm from "./Submission/SubmissionForm";
 import Head from "next/head";
 
 export default function Home() {
